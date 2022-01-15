a2b8d9378b1008ddefc2472c703241ebe3ec95e8
gunishmatta <gunish.matta@partners.rivigo.com>
2022-01-15 22:20:24 +0530

    added a factory method for deserializing the JSON response

--- app/src/main/java/com/gunish/booksearchapp/models/Book.java
+++ app/src/main/java/com/gunish/booksearchapp/models/Book.java
@@ -1,5 +1,11 @@
 package com.gunish.booksearchapp.models;
 
+import android.text.TextUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Book {
     private String openLibraryId;
     private String author;
@@ -25,6 +31,46 @@ public class Book {
     // Get large sized book cover from covers API
     public String getLargeCoverUrl() {
         return "http://covers.openlibrary.org/b/olid/" + openLibraryId + "-L.jpg?default=false";
+    }
+
+
+    public static Book fromJSON(JSONObject jsonObject)
+    {
+        Book book=new Book();
+        try {
+            if(jsonObject.has("cover_edition_key"))
+            {
+                book.openLibraryId= jsonObject.getString("cover_edition_key");
+            }
+            else if(jsonObject.has("edition_key"))
+            {
+                final JSONArray ids= jsonObject.getJSONArray("edition_key");
+                book.openLibraryId=ids.getString(0);
+            }
+            book.title=jsonObject.has("title_suggest")?jsonObject.getString("title_suggest"):"";
+            book.author = getAuthor(jsonObject);
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return book;
+    }
+
+    private static String getAuthor(JSONObject jsonObject) {
+        try {
+            final JSONArray authors = jsonObject.getJSONArray("author_name");
+            int numAuthors = authors.length();
+            final String[] authorStrings = new String[numAuthors];
+            for (int i = 0; i < numAuthors; ++i) {
+                authorStrings[i] = authors.getString(i);
+            }
+            return TextUtils.join(", ", authorStrings);
+        } catch (JSONException e) {
+            return "";
+        }
+
+
     }
 
 }
